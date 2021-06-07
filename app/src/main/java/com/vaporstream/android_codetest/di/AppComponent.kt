8549699981d7b0f7a832afec6f6ace43e6663d11
darryldjohnson <darryl.johnson.w@gmail.com>
2021-06-07 12:34:51 -0500

    Added Dagger App Module and Component

---
+++ app/src/main/java/com/vaporstream/android_codetest/di/AppComponent.kt
@@ -0,0 +1,7 @@
+package com.vaporstream.android_codetest.di
+
+import dagger.Component
+
+@Component(modules = [AppModule::class])
+class AppComponent {
+}
---
+++ app/src/main/java/com/vaporstream/android_codetest/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.vaporstream.android_codetest.di
+
+import dagger.Module
+
+@Module
+class AppModule {
+}
